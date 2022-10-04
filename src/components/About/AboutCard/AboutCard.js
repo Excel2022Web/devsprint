c7999bb724ba019a9683373f435dfe07fadc3219
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2022-10-04 13:48:00 +0530

    About page minor fixes

--- src/components/About/AboutCard/AboutCard.js
+++ src/components/About/AboutCard/AboutCard.js
@@ -11,7 +11,6 @@ function AboutCard() {
         </div>
         <h3 className='aboutcard_title'>Live Broadcast</h3>
         <p className='aboutcard_desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus massa nec graviante.</p>
-        <h6 className='aboutcard_more'>Read More</h6>
     </div>
   )
 }
