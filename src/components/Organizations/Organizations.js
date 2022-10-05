f659035a1954b49eb89058f6171e9e44f0bd8b85
Allen Shibu <93600615+alapanoski@users.noreply.github.com>
2022-10-05 23:30:38 +0530

    Add FOSS MEC description

--- src/components/Organizations/Organizations.js
+++ src/components/Organizations/Organizations.js
@@ -31,10 +31,13 @@ function Organizations() {
           <div className="orgevent2_desc">
             <h2 className="orgdesc_name">Foss MEC</h2>
             <p className="orgdesc_details">
-              The builder interface comes with a lot of options that allow you
-              to customize your building experience. We try not to lock you down
-              into specific UI conventions, but instead give you the freedom go
-              to create websites the way you want to.
+              FOSS MEC is the Free and Open-Source cell at Govt. Model
+              Engineering College, Kochi. Comprised of a bunch of enthusiastic
+              tech minds, they are passionate to learn new things while sharing
+              the knowledge they already have. By encouraging coding and
+              contributing to various open source projects, FOSS MEC aims in
+              cultivating a collaborative culture among college students and the
+              society as a whole.
             </p>
           </div>
         </div>
