692fdd7210d53f81b4f5311ad605b8f67e0f6cd6
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2022-10-06 12:32:59 +0530

    navbar

---
+++ src/components/Partners/Partners.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Partners.css'
+
+function Partners() {
+  return (
+    <div className='partners' id='partners'>Partners</div>
+  )
+}
+
+export default Partners
