827e66c787b005514e12507d7d41f9658ad721e1
Hemanth R <hemanthrt18@gmail.com>
2022-10-10 15:06:52 +0530

    timeline

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -10,15 +10,32 @@ function Timeline() {
             <div className="inner-container" >
                 <h3 className='timeLine_date'>16 October</h3>
                 <ul className="timeline">
-                    <li className="timeline-item" data-date="9:30 AM">Morning Session (Introduction to Organisation)</li>
-                    <li className="timeline-item" data-date="11:30 AM">Move to allotted classrooms</li>
-                    <li className="timeline-item" data-date="12:30 PM">Lunch</li>
-                    <li className="timeline-item" data-date="1:30 PM">Dive into the Organisation</li>
-                    <li className="timeline-item" data-date="4:30 PM">Closing Ceremony</li>
+                    <li className="timeline-item" data-date="9:30 AM">
+                    <span className='t_bold'>Introduction to the Organisations</span>
+                    <span>The programme commences with a morning session where all the participating projects are introduced to the participants by the Project Maintainers & Representatives. </span>
+                    </li>
+                    <li className="timeline-item" data-date="11:30 AM">
+                      <span className='t_bold'>Branch Off</span>
+                      <span>After choosing the projects of interest, participants approach the project representatives, at their designated locations, to get a more comprehensive view of the project and to contribute to it.</span>
+                    </li>
+                    <li className="timeline-item" data-date="12:30 PM">
+                      <span className='t_bold'>Lunch Break</span>
+                    </li>
+                    <li className="timeline-item" data-date="1:30 PM">
+                      <span className='t_bold'>Dive into the Organisation </span>
+                      <span>Participants get acquainted well enough to start their OSS Contributions on the go. Contributions of all mastery levels promoted, from feature adds, redesigning a whole webpage to writing the documentation.</span>
+                    </li>
+                    <li className="timeline-item" data-date="4:30 PM">
+                      <span className='t_bold'>Closing Ceremony</span>
+                      <span>The Inaugural Day of DevSprint officially concludes.</span>
+                    </li>
                 </ul>
                 <h3 className='timeLine_date'>17 - 23 October</h3>
                 <ul className="timeline">
-                    <li className="timeline-item" data-date="">Remote contributions</li>
+                    <li className="timeline-item" data-date="">
+                      <span className='t_bold'>Remote contributions</span>
+                      <span>Participants complete their contributions remotely, over the period of a week, concluding on 23rd October.</span>
+                    </li>
                 </ul>
             </div>
             </div>
